29dd490f30603a1a3f0be41978a277c1ecfb2328
Zopper9000 <david.lu@hotmail.com>
2015-09-02 19:37:31 -0400

    added hand ranking

--- comparehands.m
+++ comparehands.m
@@ -9,70 +9,13 @@
 %-1 if first hand loses
 %0 if pot is split
 function winner = comparehands(a, b)
-%Extract raw card values, returning integer between 1 and 13
-avalues = mod(a - 1, 13) + 1;
-bvalues = mod(b - 1, 13) + 1;
 
-%Extract raw suits, returning integer between 1 and 4
-asuits = floor(a / 13 + 1);
-bsuits = floor(b / 13 + 1);
-
-disp('Hand A:');
-disp(avalues);
-disp(asuits);
-disp('Hand B:');
-disp(bvalues);
-disp(bsuits);
-
-%Check for straight
-astraight = sort(avalues);
-for i = 2:5
-    if(astraight(i) - astraight(1) ~= i - 1)
-       astraight = false; 
-       break;
-    end
-end
-if(astraight ~= false)
-   astraight = true; 
-end
-
-disp('Straight A:');
-disp(astraight);
-
-bstraight = sort(bvalues);
-for i = 2:5
-    if(bstraight(i) - bstraight(1) ~= i - 1)
-       bstraight = false; 
-       break;
-    end
-end
-if(bstraight ~= false)
-   bstraight = true; 
-end
-
-disp('Straight B:');
-disp(bstraight);
-
-%Check for flush
-aflush = sort(asuits);
-if(aflush(5) - aflush(1) == 0)
-    aflush = true;
+if(handrank(a) > handrank(b))
+   winner = 1;
+elseif(handrank(b) > handrank(a))
+    winner = -1;
 else
-    aflush = false;
-end
-
-disp('Flush A:');
-disp(aflush);
-
-bflush = sort(bsuits);
-if(bflush(5) - bflush(1) == 0)
-    bflush = true;
-else
-    bflush = false;
-end
-
-disp('Flush B:');
-disp(bflush);
     
-winner = 0;
+end
+    
 end
---
+++ handrank.m
@@ -0,0 +1,81 @@
+%Ranks a 5 card poker hand
+%All cards represented as an integer from 1-52
+%Standard Texas Hold 'em hands used
+%Returns:
+%High Card - 1
+%Pair - 2
+%Two pair - 3
+%Three of a Kind - 4
+%Straight - 5
+%Flush - 6
+%Full House - 7
+%Four of a Kind - 8
+%Straight Flush - 9
+function rank = handrank(a)
+
+    %Extract raw card values, returning integer between 1 and 13
+    values = mod(a - 1, 13) + 1;
+
+    %Extract raw suits, returning integer between 1 and 4
+    suits = ceil(a / 13);
+
+    disp('Hand:');
+    disp(values);
+    disp(suits);
+
+    %Check for straight
+    straight = sort(values);
+    for i = 2:5
+        if(straight(i) - straight(1) ~= i - 1)
+           straight = false; 
+           break;
+        end
+    end
+    if(straight ~= false)
+       straight = true; 
+    end
+
+    %Check for flush
+    flush = sort(suits);
+    if(flush(5) - flush(1) == 0)
+        flush = true;
+    else
+        flush = false;
+    end
+
+    %Straight Flush
+    if(flush + straight == 2)
+        rank = 9;
+    %Flush only
+    elseif(flush == true && straight == false)
+       rank = 6;
+    %Straight only
+    elseif(flush == false && straight == true)
+       rank = 5;
+    %Check for other hands, destroying suit information
+    else
+        values = sort(values);
+    
+        %4oaK, difference between largest and 4th largest is 0 or difference
+        %between 2nd and 5th is 0
+        if(values(5) - values(2) == 0 || values(4) - values(1) == 0)
+            rank = 8;
+        %FH, difference between largest and 3rd is 0, and smallest and 4th is
+        %0, or other way around
+        elseif((values(5) - values(3) == 0 && values(2) - values(1) == 0) || (values(5) - values(4) == 0 && values(3) - values(1) == 0))
+            rank = 7;
+        %3oaK, difference between any 3 is 0
+        elseif(values(5) - values(3) == 0 || (values(4) - values(2) == 0 || values(3) - values(1) == 0))
+            rank = 4;
+        %2P, finding 2 pairs
+        elseif((values(5) - values(4) == 0 && values(3) - values(2) == 0) || ((values(4) - values(3) == 0 && values(2) - values(1) == 0) || (values(5) - values(4) == 0 && values(2) - values(1) == 0)))
+            rank = 3;
+        %1P, finding 1 pair
+        elseif((values(5) - values(4) == 0 || values(4) - values(3) == 0) || (values(3) - values(2) == 0 || values(2) - values(1) == 0))
+            rank = 2;
+        %High Card
+        else
+            rank = 1;
+        end
+    end
+end
