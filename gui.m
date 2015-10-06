ee267b8ae5a9c76bfb973465e7a67b06cd6d1dca
Zopper9000 <david.lu@hotmail.com>
2015-10-06 11:00:46 -0400

    I have no idea what I did

--- gui.m
+++ gui.m
@@ -99,5 +99,5 @@ function listbox1_CreateFcn(hObject, eventdata, handles)
 % Hint: listbox controls usually have a white background on Windows.
 %       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','black');
+    set(hObject,'BackgroundColor','white');
 end
